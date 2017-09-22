3b14b618636534dbe570eff1b000fa25f95390b6
rwatson <randy.watson@merrillcorp.com>
2017-09-22 14:25:26 -0500

    nullable types

--- src/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
+++ src/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
@@ -22,12 +22,20 @@ fun todoTask7(client: Client?, message: String?, mailer: Mailer): Nothing = TODO
     references = { JavaCode7().sendMessageToClient(client, message, mailer) }
 )
 
-fun sendMessageToClient(
-        client: Client?, message: String?, mailer: Mailer
-) {
-    todoTask7(client, message, mailer)
+//the ? indicates that the client and message are nullable, mailer is not
+fun sendMessageToClient(client: Client?, message: String?, mailer: Mailer) {
+    val personalInfo: PersonalInfo? = client?.personalInfo//note if client is null, then this whole thing evals to null without an NPE
+    val email: String? = client?.personalInfo?.email
+  //my solution was very close.  I pulled out the vars above, and the answer, they pull out just the email and use the following if
+    //if (email != null && message != null) {
+    //since the ? will prevent an NPE, it makes the result null thats why the above line works
+    if (client == null || message == null || personalInfo == null || email == null) {
+        return
+    }
+    mailer.sendMessage(email, message)
 }
 
+//interesting these are used in the JavaCOde
 class Client (val personalInfo: PersonalInfo?)
 class PersonalInfo (val email: String?)
 
