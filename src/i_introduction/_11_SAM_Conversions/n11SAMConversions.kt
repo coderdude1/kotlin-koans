1d7a7d6580c5d15feb7736ecc8a09463ac2696d9
rwatson <randy.watson@merrillcorp.com>
2017-09-25 13:19:09 -0500

    SAM (Single Abstract Method) in stead of Object Expression

--- src/i_introduction/_11_SAM_Conversions/n11SAMConversions.kt
+++ src/i_introduction/_11_SAM_Conversions/n11SAMConversions.kt
@@ -9,12 +9,14 @@ fun todoTask11(): Nothing = TODO(
         Task 11.
         When an object implements a SAM interface (one with a Single Abstract Method), you can pass a lambda instead.
         Rewrite the previous example changing an object expression to a lambda.
+
+        note intellij suggested this approach in previous example
     """,
     documentation = doc11()
 )
 
 fun task11(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, { x, y -> todoTask11() })
+    Collections.sort(arrayList, { x, y -> y - x})
     return arrayList
 }
