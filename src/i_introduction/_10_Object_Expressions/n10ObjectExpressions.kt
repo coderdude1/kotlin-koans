8a0fdaf5000d0e08a46d4411379899e70445d592
rwatson <randy.watson@merrillcorp.com>
2017-09-25 13:16:25 -0500

    Object Expressions

--- src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
@@ -3,6 +3,7 @@ package i_introduction._10_Object_Expressions
 import util.TODO
 import util.doc10
 import java.util.*
+import kotlin.Comparator
 
 fun todoTask10(): Nothing = TODO(
     """
@@ -12,12 +13,30 @@ fun todoTask10(): Nothing = TODO(
         Add an object expression that provides a comparator to sort a list in a descending order using 'java.util.Collections' class.
         In Kotlin you use Kotlin library extensions instead of java.util.Collections,
         but this example is still a good demonstration of mixing Kotlin and Java code.
+
+        https://kotlinlang.org/docs/reference/object-declarations.html
+
     """,
     documentation = doc10()
 )
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, todoTask10())
+    Collections.sort(arrayList, object : Comparator<Int> {  //I had to peek, this is really the same as passing in an anonymous class
+        override fun compare(x: Int, y: Int) = y - x
+    })
     return arrayList
 }
+
+/*
+solution in answer branch.  when using intellij it made stuff nullable, I removed that
+    Collections.sort(arrayList, object : Comparator<Int> {  //I had to peek, this is really the same as passing in an anonymous class
+        override fun compare(x: Int, y: Int) = y - x
+    })
+
+    intellij suggested using lambda instead of object : Comparator<Int>
+    recomended this instead (note it is not an object expression):
+        Collections.sort(arrayList) { x, y ->
+        y - x
+    }
+ */
