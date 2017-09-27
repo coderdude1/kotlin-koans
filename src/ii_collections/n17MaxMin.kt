59493c4d6b9c24d01fcf55a58ab220e3d1ebc7d2
rwatson <randy.watson@merrillcorp.com>
2017-09-27 15:21:39 -0500

    max and min

--- src/ii_collections/n17MaxMin.kt
+++ src/ii_collections/n17MaxMin.kt
@@ -2,15 +2,17 @@ package ii_collections
 
 fun example4() {
     val max = listOf(1, 42, 4).max()
-    val longestString = listOf("a", "b").maxBy { it.length }
+    val longestString = listOf("a", "bc").maxBy { it.length }
 }
 
 fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? {
     // Return a customer whose order count is the highest among all customers
-    todoCollectionTask()
+//    return customers.maxWith(Comparator { o1, o2 -> o1.orders.size - o2.orders.size })
+    return customers.maxBy { it.orders.size } //from answer
+
 }
 
 fun Customer.getMostExpensiveOrderedProduct(): Product? {
     // Return the most expensive product which has been ordered
-    todoCollectionTask()
+    return orderedProducts.maxBy { it.price }
 }
