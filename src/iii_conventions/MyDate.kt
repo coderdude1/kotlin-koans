58b20350a2850c3f1d47b8be95b8dc10c4be80e9
Randy <drbugsmn@gmail.com>
2017-10-09 23:10:52 -0500

    comparable my answer that works but is very very java like, need to find a better kotlin solution that I know exists

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,7 +1,20 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        //very java way, this works gonna try to kotlinfy it before checking the actual answer
+        var diff = 0
+        if (year != other.year ) //not sure about a when block since the test is cascading on a different property for each block
+            diff = year - other.year
+        else if(month != other.month)
+            diff = month - other.month;
+        else
+            diff = dayOfMonth - other.dayOfMonth
+        return diff
+    }
+}
 
+//Functions that overload operators need to be marked with the operator modifier.
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
 enum class TimeInterval {
