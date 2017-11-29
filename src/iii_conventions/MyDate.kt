814e16803d11762a0493cacbec9b38d34d0dcaa6
rwatson <randy.watson@merrillcorp.com>
2017-11-29 13:35:49 -0600

    n28 created custom MyDate iterator

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -19,9 +19,20 @@ enum class TimeInterval {
     YEAR
 }
 
-//This works too
+//This works too for the .. n26InRange
 //class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>
-class DateRange(val start: MyDate, val endInclusive: MyDate) {
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterator<MyDate> { //Itereator object Expression
+    var currentDate: MyDate = start
+
+    override fun hasNext(): Boolean = currentDate <= endInclusive  //expressionSyntax
+
+
+    override fun next(): MyDate {
+        val retval = currentDate
+        currentDate = currentDate.addTimeIntervals(TimeInterval.DAY, 1)
+        return retval
+    }
+
     //Intellij offers to turn this into a rangeCheck myDate in start..endInclusive
     operator fun contains(myDate: MyDate) : Boolean = start <= myDate && myDate <= endInclusive
 }
