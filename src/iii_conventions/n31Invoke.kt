f1987773b0fb689e3d699bcbac7b1dc5c04bf101
rwatson <randy.watson@merrillcorp.com>
2018-11-16 12:42:18 -0600

    learn the invoke function

--- src/iii_conventions/n31Invoke.kt
+++ src/iii_conventions/n31Invoke.kt
@@ -3,7 +3,27 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable(var invocations: Int = 0) {
+    /*
+    The invoke() operator allows instance of your classes to be called as functions.  Note lambdas use this! so we do
+            val square = { value: Int -> value * value }
+
+            println(square(5)) // prints 25
+
+        intesd of
+            println(square.invoke(5)) // also prints 25
+
+            https://proandroiddev.com/til-about-operator-overloading-in-kotlin-and-the-invoke-operator-da3686ef4694
+
+    operator is required since we are overloading the default invoke fun
+     */
+    operator fun invoke(): Invokable {
+        invocations++
+        return this
+    }
+
+    fun getNumberOfInvocations() = invocations
+}
 
 fun todoTask31(): Nothing = TODO(
     """
@@ -14,6 +34,6 @@ fun todoTask31(): Nothing = TODO(
     references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+//    todoTask31()
+    return invokable()()()().getNumberOfInvocations()
 }
